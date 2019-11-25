1fc564a7f963556c928bd91daeb5ae38cdafa367
Dean <wang.deans@hotmail.com>
2019-11-25 15:52:42 +0800

    更新 zhihu_answer.js

--- JS/Zhihu-ad-answer.js
+++ JS/Zhihu-ad-answer.js
@@ -2,13 +2,20 @@
  * @supported 2ABDBE39B3FF 8BCC0A25D731
  */
 
+//regex: ^https://api.zhihu.com/.*/questions
+//Original author: onewayticket255
+
 let body = $response.body
 body=JSON.parse(body)
+if (body['ad_info']) {
 delete body['ad_info']
+};
+if (body.data) {
 body['data'].forEach((element, index)=> {
     if(element['author']['name']=="盐选推荐"){ 
           body['data'].splice(index,1)  
      }
- })
+  })
+ };
 body=JSON.stringify(body)
 $done({body})
